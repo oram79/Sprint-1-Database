e3206b52a2f9c4bae2c3c1500a879bf1f44dea5a
Logan Oram <logan.oram@keyin.com>
2024-11-01 12:05:06 -0230

    mistake made with index.js

--- Src/index.js
+++ Src/index.js
@@ -1,37 +1,106 @@
-const { Restaurants, Cuisines } = require("./utils/data");
-const express = require('express');
-const path = require('path');
-const { generateRandomMenuItem, generateMenu, selectRandomCuisine } = require("./utils/restaurantUtils");
+const { Pool } = require('pg');
 
-const app = express();
-let restaurantData = {}; //This should be populated soon
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
+// PostgreSQL connection
+const pool = new Pool({
+  user: 'postgres', //This _should_ be your username, as it's the default one Postgres uses
+  host: 'localhost',
+  database: 'your_database_name', //This should be changed to reflect your actual database
+  password: 'your_database_password', //This should be changed to reflect the password you used when setting up Postgres
+  port: 5432,
+});
 
 /**
- * GET /
- * Renders the homepage that lists cities and restaurant names.
+ * Creates the database tables, if they do not already exist.
  */
-app.get('/', (request, response) => {
-    response.render('index', { restaurants: Restaurants });
-  });
-  
-  /**
-   * GET /restaurant/:name
-   * Displays a specific restaurant's random menu.
-   * The cuisine is randomly selected and a menu is generated based on it.
-   */
-  app.get('/restaurant', (request, response) => {
-    const restaurantId = request.query.restaurantId;
-    console.log(`restaurantId: ${restaurantId}`);
-    //Get the restaurants menu, and then display the page
-  });
+async function createTable() {
+  // TODO: Add code to create Movies, Customers, and Rentals tables
+};
 
-  //Add any other required routes here
+/**
+ * Inserts a new movie into the Movies table.
+ * 
+ * @param {string} title Title of the movie
+ * @param {number} year Year the movie was released
+ * @param {string} genre Genre of the movie
+ * @param {string} director Director of the movie
+ */
+async function insertMovie(title, year, genre, director) {
+  // TODO: Add code to insert a new movie into the Movies table
+};
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+/**
+ * Prints all movies in the database to the console
+ */
+async function displayMovies() {
+  // TODO: Add code to retrieve and print all movies from the Movies table
+};
+
+/**
+ * Updates a customer's email address.
+ * 
+ * @param {number} customerId ID of the customer
+ * @param {string} newEmail New email address of the customer
+ */
+async function updateCustomerEmail(customerId, newEmail) {
+  // TODO: Add code to update a customer's email address
+};
+
+/**
+ * Removes a customer from the database along with their rental history.
+ * 
+ * @param {number} customerId ID of the customer to remove
+ */
+async function removeCustomer(customerId) {
+  // TODO: Add code to remove a customer and their rental history
+};
+
+/**
+ * Prints a help message to the console
+ */
+function printHelp() {
+  console.log('Usage:');
+  console.log('  insert <title> <year> <genre> <director> - Insert a movie');
+  console.log('  show - Show all movies');
+  console.log('  update <customer_id> <new_email> - Update a customer\'s email');
+  console.log('  remove <customer_id> - Remove a customer from the database');
+}
+
+/**
+ * Runs our CLI app to manage the movie rentals database
+ */
+async function runCLI() {
+  await createTable();
+
+  const args = process.argv.slice(2);
+  switch (args[0]) {
+    case 'insert':
+      if (args.length !== 5) {
+        printHelp();
+        return;
+      }
+      await insertMovie(args[1], parseInt(args[2]), args[3], args[4]);
+      break;
+    case 'show':
+      await displayMovies();
+      break;
+    case 'update':
+      if (args.length !== 3) {
+        printHelp();
+        return;
+      }
+      await updateCustomerEmail(parseInt(args[1]), args[2]);
+      break;
+    case 'remove':
+      if (args.length !== 2) {
+        printHelp();
+        return;
+      }
+      await removeCustomer(parseInt(args[1]));
+      break;
+    default:
+      printHelp();
+      break;
+  }
+};
+
+runCLI();
